78ee7e85aa55353dc94a1dc8228a2d682e9dd8a2
Neathan Kungu <natekungu857@gmail.com>
2025-07-30 09:37:11 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import { username } from "../data/user";
+import { city } from "../data/user";
+import { image } from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image}/>
     </div>
   );
 }
